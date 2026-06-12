9357d0f8531ed47292b863de90b11c9716e61bc6
ramamastieri13 <rmastierilopez@uade.edu.ar>
2026-06-11 21:29:15 -0300

    Creacion de los objetos calle y interseccion. Modificacion en pila.

--- src/modelo/Calle.java
+++ src/modelo/Calle.java
@@ -1,4 +1,65 @@
 package modelo;
 
 public class Calle {
+
+    private String id;
+    private String nombre;
+    private String interseccionOrigen;
+    private String interseccionDestino;
+    private boolean congestionada;
+
+    public Calle(String id, String nombre, String interseccionOrigen, String interseccionDestino) {
+        this.id = id;
+        this.nombre = nombre;
+        this.interseccionOrigen = interseccionOrigen;
+        this.interseccionDestino = interseccionDestino;
+        this.congestionada = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getInterseccionOrigen() {
+        return interseccionOrigen;
+    }
+
+    public void setInterseccionOrigen(String interseccionOrigen) {
+        this.interseccionOrigen = interseccionOrigen;
+    }
+
+    public String getInterseccionDestino() {
+        return interseccionDestino;
+    }
+
+    public void setInterseccionDestino(String interseccionDestino) {
+        this.interseccionDestino = interseccionDestino;
+    }
+
+    public boolean isCongestionada() {
+        return congestionada;
+    }
+
+    public void setCongestionada(boolean congestionada) {
+        this.congestionada = congestionada;
+    }
+
+    @Override
+    public String toString() {
+        return "Calle " + id + " | " + nombre +
+                " | De: " + interseccionOrigen + " a: " + interseccionDestino +
+                " | Congestionada: " + congestionada;
+    }
 }
--- src/modelo/Interseccion.java
+++ src/modelo/Interseccion.java
@@ -1,4 +1,65 @@
 package modelo;
 
+import tda.ListaEnlazada;
+
 public class Interseccion {
+
+    private String id;
+    private String nombre;
+    private ListaEnlazada<Semaforo> semaforos;
+    private ListaEnlazada<Camara> camaras;
+    private ListaEnlazada<Vehiculo> vehiculosEsperando;
+
+    public Interseccion(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.semaforos = new ListaEnlazada<>();
+        this.camaras = new ListaEnlazada<>();
+        this.vehiculosEsperando = new ListaEnlazada<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ListaEnlazada<Semaforo> getSemaforos() {
+        return semaforos;
+    }
+
+    public void setSemaforos(ListaEnlazada<Semaforo> semaforos) {
+        this.semaforos = semaforos;
+    }
+
+    public ListaEnlazada<Camara> getCamaras() {
+        return camaras;
+    }
+
+    public void setCamaras(ListaEnlazada<Camara> camaras) {
+        this.camaras = camaras;
+    }
+
+    public ListaEnlazada<Vehiculo> getVehiculosEsperando() {
+        return vehiculosEsperando;
+    }
+
+    public void setVehiculosEsperando(ListaEnlazada<Vehiculo> vehiculosEsperando) {
+        this.vehiculosEsperando = vehiculosEsperando;
+    }
+
+    @Override
+    public String toString() {
+        return "Interseccion " + id + " | Nombre: " + nombre;
+    }
 }
